075f1050f12ecb7fa3b6bb0748f11d2229ae9cf6
Luis Fernando Machado <>
2023-02-17 14:19:57 -0300

    first stage of the flipper animation

--- app/src/main/java/com/materialdesign/viewflipper/MainActivity.java
+++ app/src/main/java/com/materialdesign/viewflipper/MainActivity.java
@@ -1,14 +1,34 @@
 package com.materialdesign.viewflipper;
 
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ViewFlipper;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+	private ViewFlipper view_flipper;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		view_flipper = findViewById(R.id.view_flipper);
+		TextView textView = new TextView(this);
+		textView.setText("Dynamically added TextView");
+		textView.setGravity(Gravity.CENTER);
+		view_flipper.addView(textView);
+	}
+
+	public void previousView(View v) {
+		view_flipper.showPrevious();
+	}
+
+	public void nextView(View v) {
+		view_flipper.showNext();
 	}
 }
